192a68c4f8abe66d82968b169fa1cdf034ecf4e9
Deborah Barcenas <deborah.barcenas@gmail.com>
2019-03-21 22:14:04 +0000

    Automatically backed up by Learn

--- my_first_hash.rb
+++ my_first_hash.rb
@@ -1,7 +1,9 @@
 def my_hash
-  # use the literal constructor to set the variable, my_hash, equal to a hash with key/value pairs of your choice.
-
-  
+  my_hash = {
+    "loki" => "Best dog in the world",
+    "otters" => "are very silly animals",
+    "animals" => "Love them more than humans"
+  }
 end
 
 
